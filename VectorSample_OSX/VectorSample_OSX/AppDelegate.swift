02c6bd2150c06c7a861c2c64481f7b7289ddefb8
Masayuki Ono <ono@ripplex.com>
2014-10-02 09:15:17 +0900

    サンプル作成

---
+++ VectorSample_OSX/VectorSample_OSX/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  VectorSample_OSX
+//
+//  Created by mono on 10/2/14.
+//  Copyright (c) 2014 Mono. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
